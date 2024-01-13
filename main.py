fb6cb8e70fafd42fa9edd68890fb2176152c29f9
Oleg <InferKing@users.noreply.github.com>
2024-01-13 11:54:16 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import g4f
+import customtkinter as ctk
+import threading as th
+
+messages = []
+
+temp = None
+ctk.set_appearance_mode("dark")
+app = ctk.CTk()
+app.geometry("560x320")
+app.resizable(False, True)
+app.grid_columnconfigure(0, weight=3)
+app.grid_columnconfigure(1, weight=0)
+
+
+def change_text(text, is_human):
+    if is_human:
+        res = "Вы: " + str(text) + "\n"
+    else:
+        res = "ChatGPT: " + str(text) + "\n"
+    textbox.configure(state="normal")
+    textbox.insert("end", res)
+    textbox.configure(state="disabled", require_redraw=True)
+
+
+def wait_response():
+    global temp
+    try:
+        response = g4f.ChatCompletion.create(
+            model=g4f.models.gpt_4,
+            messages=messages,
+            provider=g4f.Provider.GeekGpt
+        )
+    except Exception as e:
+        print(e)
+        response = "Попробуйте написать снова."
+    temp = response
+    app.event_generate("<<TaskCompleted>>")
+
+
+def handle_task(event):
+    change_text(temp, False)
+    messages.append({"role": "assistant", "content": temp})
+
+
+def btn_callback():
+    change_text(entry.get(), True)
+    messages.append({"role": "user", "content": entry.get()})
+    entry.delete(0, 'end')
+    thread = th.Thread(target=wait_response)
+    thread.start()
+
+
+lbl = ctk.CTkLabel(app, text="Чат с нейросетью", height=30, font=("Helvetica", 24))
+lbl.grid(row=0, column=0, columnspan=2, sticky="ew", pady=(10, 10))
+textbox = ctk.CTkTextbox(app, state="disabled")
+textbox.grid(row=1, column=0, padx=(10, 10), pady=(12, 0), columnspan=2, sticky="ew")
+entry = ctk.CTkEntry(app, height=34, placeholder_text="Введи сообщение")
+entry.grid(row=2, column=0, pady=(12, 0), padx=(10, 10), sticky="ew")
+btn = ctk.CTkButton(app, text="Отправить", height=34, command=btn_callback)
+btn.grid(row=2, column=1, pady=(12, 0), padx=(10, 10), sticky="ew")
+app.bind("<<TaskCompleted>>", handle_task)
+app.mainloop()
